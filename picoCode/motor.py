7bbbd93f33666493172b05ddbd0a6e63b6d14438
cbhaygoo <cbhaygoo@ncsu.edu>
2026-02-28 14:16:56 -0500

    Fixing motor controller

--- picoCode/motor.py
+++ picoCode/motor.py
@@ -10,7 +10,6 @@ class Motor:
     def move(self, direction, steps):
         # Move motor in a direction (1=open, 0=close)
         # Returns 'DONE', 'STALL', or 'TIMEOUT'
-        self.driver.wake()
         self.driver.enable()
         self.driver.dir.value(direction)
         time.sleep_ms(5)
@@ -18,7 +17,6 @@ class Motor:
         for i in range(steps):
             if self.driver.is_stalled():
                 self.driver.disable()
-                self.driver.sleep()
                 return "STALL"
 
             self.driver.step.value(1)
@@ -27,10 +25,8 @@ class Motor:
             time.sleep_us(self.step_delay_us // 2)
 
         self.driver.disable()
-        self.driver.sleep()
         return "DONE"
 
     def stop(self):
         # Immediately stop and disable motor
         self.driver.disable()
-        self.driver.sleep()
